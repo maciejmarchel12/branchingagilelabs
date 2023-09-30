6236c63fd39214bbdf31ebdc942b5e703f28136d
Diarmuid O Connor <doconnor@wit.ie>
2023-09-30 12:50:53 +0100

    Implemented and tested removeProductById"

--- src/productCatalogue.js
+++ src/productCatalogue.js
@@ -16,4 +16,14 @@ export class Catalogue {
     }
     return false;
   }
+
+  removeProductById(id) {
+    const removedProduct = this.findProductById(id);
+    if (removedProduct) {
+      this.products = this.products.filter(
+        (product) => product.id !== id // ***********
+      );
+    }
+    return removedProduct;
+  }
 }

a639c8e89440317f99f07eb6d6f23509c8ba87f6
maciejmarchel12 <maciejmarchel14@gmail.com>
2023-10-17 10:47:15 +0100

    Refactored check reorder

--- src/productCatalogue.js
+++ src/productCatalogue.js
@@ -29,12 +29,10 @@ export class Catalogue {
 
   checkReorders() {
     const result = { type: "Reorder", productIds: [] };
-   this.products.forEach( (p) => {
-     if (p.quantityInStock <= p.reorderLevel) {
-       result.productIds.push(p.id);
-     }
-   });
-   return result;
- }
+    result.productIds = this.products
+      .filter((p) => p.quantityInStock < p.reorderLevel)
+      .map((p) => p.id);
+    return result;
+  }
 
 }
